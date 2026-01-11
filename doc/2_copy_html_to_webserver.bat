460412f7890e518f4fb366663fddd35ef6703800
yramgu <46901691+yramgu@users.noreply.github.com>
2026-01-11 16:13:09 +0000

    Delete doc directory

--- doc/2_copy_html_to_webserver.bat
+++
@@ -1,8 +0,0 @@
-rem This will copy the html build to the published location
-rem Kept as a separate script to let users experiment without taking
-rem down released version. Should be run once happy with latest build
-rem Keith 13th June 2019
-
-xcopy /Y/E build\html\* \\shareduk\Projects\docs\your_location\
-
-rem pause
